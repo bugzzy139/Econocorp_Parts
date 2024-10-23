0e0f2b0a7a1052f7aef91adde38174971389b966
Marcus Johnson <bugzzy139@gmail.com>
2024-10-23 17:00:03 -0400

    Improved Searching
    Searching is a lot better now. The search bar is now a form, so pressing Enter will submit the search data and no longer requires the user to press the search button. If a search doesn't have anything to show, it will display an alert that tells the user it couldn't find what they searched for. When searching, it will smoothly scroll to the first instance of the search. The header, which contains the search bar, has been made sticky so that it follows as the page scrolls, allowing for easier navigation to add more searches.

--- JS/search.js
+++ JS/search.js
@@ -1,6 +1,3 @@
-function getSearchString(){
-    return document.getElementById('search-bar').value;
-}
 
 function searchThroughContainers(searchString){
     let containerArray = document.getElementsByClassName('container');
@@ -8,6 +5,8 @@ function searchThroughContainers(searchString){
     searchString = searchString.toUpperCase();
 
     console.log(searchString);
+
+    let matchedResults = [];
 
     for(let i = 0; i < containerArray.length; i++){
         let container = containerArray[i];
@@ -19,20 +18,30 @@ function searchThroughContainers(searchString){
             if(container.children[j].innerHTML.includes(searchString)){
                 //Success
                 toggleItemDisplay(container);
+                matchedResults.push(container);
             }
         }       
     }
+
+    //We didn't find any matching results
+    if(matchedResults.length === 0){
+        window.alert("No matches found.");
+        return;
+    }
+
+    //Scroll to first instance of item
+    matchedResults[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest"});
 }
 
 function search(){
-    let searchString = getSearchString();
-
+    let searchString = document.getElementById('search-bar').value;
     //select all items that have elements that match the search string
     searchThroughContainers(searchString);
 }
 
-let searchButton = document.getElementById('search-btn');
+let searchForm = document.getElementById('searchForm');
 
-searchButton.addEventListener('click', () => {
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
     search();
 });
